9c8c5fbdcd1e6b0e79abe01aad349dd2bd0a8829
Silas Köhnke <silaskoehnke@gmx.de>
2024-06-02 15:06:18 +0200

    change dadbod config

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -1,5 +1,26 @@
+-- return {
+--   'tpope/vim-dadbod',
+--   'kristijanhusak/vim-dadbod-completion',
+--   'kristijanhusak/vim-dadbod-ui',
+--}
 return {
-  'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-completion',
   'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_show_database_icon = 1
+    vim.g.db_ui_auto_execute_table_helpers = 1
+    vim.g.db_ui_winwidth = 50
+    vim.g.db_ui_execute_on_save = 0
+  end,
 }
